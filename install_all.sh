e33323f29cbb4b8c07e18acbe3f7fed1c79539da
xhrist14n <xhrist14n@gmail.com>
2016-11-20 15:39:59 -0500

    Create install_all.sh
    install all in ubuntu

---
+++ install_all.sh
@@ -0,0 +1,35 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y build-essential
+sudo apt-get install -y openjdk-7-jre 
+sudo apt-get install -y openjdk-7-jdk
+sudo apt-get install -y codeblocks
+sudo apt-get install -y vlc
+sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install -y python-virtualenv curl
+sudo pip install nodeenv
+sudo apt-get install -y libopencv-dev libopencv-ml-dev libopencv-core-dev  libopencv-contrib-dev  python-opencv 
+sudo apt-get install -y libreoffice
+sudo apt-get install -y gimp
+sudo apt-get install -y chromium-browser
+sudo apt-get install -y libgtk-3-dev build-essential checkinstall
+sudo apt-get install -y libwxgtk2.8-dev libwxgtk2.8-dbg
+sudo apt-get install -y libmysqlclient-dev
+sudo apt-get install -y libmysqld-dev
+sudo apt-get install -y gcc-multilib
+sudo apt-get install -y rar
+sudo apt-get install -y ntfs-config testdisk scrounge-ntfs
+sudo apt-get install -y ntfs-3g
+sudo apt-get install -y testdisk
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+sudo apt-get install -y p7zip-full
+sudo apt-get install -y mysql-workbench
+sudo apt-get install -y curl
+sudo apt-get install -y libpq-dev python-dev
+sudo apt-get install -y filezilla
+sudo apt-get install -y texlive-full
+sudo apt-get install -y texmaker
+sudo apt-add-repository ppa:blahota/texstudio
+sudo apt-get update
+sudo apt-get install -y texstudio
