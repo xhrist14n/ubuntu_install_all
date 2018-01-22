f5ddc0ab4463c821dc586982529cfeff3dfd6ef8
xhrist14n <xhrist14n@gmail.com>
2018-01-21 23:15:32 -0500

    composer install
    composer install by command line

---
+++ install_composer.sh
@@ -0,0 +1,12 @@
+sudo apt-get update 
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+
+
+sudo apt-get install zip gzip unzip tar git curl wget -y
+sudo apt-get install php-cli php-zip php-gettext libapache2-mod-php php-xml php-mbstring -y
+
+sudo phpenmod mcrypt
+sudo phpenmod mbstring
+
+sudo apt-get install composer -y
