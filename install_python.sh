febcc56c1ac439bd303e73fd19801632af99e5b8
xhrist14n <xhrist14n@gmail.com>
2018-02-16 23:34:06 -0500

    install python
    install python

---
+++ install_python.sh
@@ -0,0 +1,22 @@
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y libmysqlclient-dev
+sudo apt-get install -y libmysqld-dev
+sudo apt-get install -y gcc-multilib
+sudo apt-get install -y rar
+sudo apt-get install -y zip
+sudo apt-get install -y curl
+sudo apt-get install -y libpq-dev python-dev
+sudo apt-get install -y python-pip
+sudo snap install  heroku --classic
+sudo pip install pipenv
+sudo apt-get install -y python3-pip
+sudo pip3 install pipenv
+wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tgz
+tar -xvf Python-3.6.3.tgz
+cd Python-3.6.3
+./configure
+make
+sudo make install
+sudo apt-get install -y zlib1g-dev
+
